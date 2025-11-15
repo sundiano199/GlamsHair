41df76f044c1e10d8e3e76d3c03ec87671688066
sunday <iamdyclef@gmail.com>
2025-11-15 03:43:09 +0100

    feat:done with signin and signup UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -32,6 +32,7 @@ const SignUp = () => {
         <p className="text-red-500 text-2xl text-left">
           {errors.fullName?.message}
         </p>
+
         <input
           type="text"
           placeholder="Full name"
@@ -42,6 +43,7 @@ const SignUp = () => {
         <p className="text-red-500 text-2xl text-left">
           {errors.email?.message}
         </p>
+
         <input
           type="email"
           name="email"
@@ -87,48 +89,51 @@ const SignUp = () => {
         <p className="text-red-500 text-2xl text-left">
           {errors.password?.message}
         </p>
-
-        <input
-          type={passwordVisible ? "text" : "password"}
-          name="password"
-          id=""
-          placeholder="Password"
-          className=" border shadow rounded-xl py-8 mb-8 text-4xl placeholder:text-4xl px-5 placeholder:px-5 placeholder:text-gray-400 bg-white "
-          {...register("password")}
-        />
-        {/* Hide password for password */}
-        <div
-          className="absolute right-8 top-110"
-          onClick={() => setPasswordVisible((v) => !v)}
-        >
-          {passwordVisible ? (
-            <IoMdEyeOff className="text-6xl invert-50" />
-          ) : (
-            <IoMdEye className="text-6xl invert-50" />
-          )}
+        <div className="relative w-full">
+          <input
+            type={passwordVisible ? "text" : "password"}
+            name="password"
+            id=""
+            placeholder="Password"
+            className=" w-full border shadow rounded-xl py-8 mb-8 text-4xl placeholder:text-4xl px-5 placeholder:px-5 placeholder:text-gray-400 bg-white "
+            {...register("password")}
+          />
+          {/* Hide password for password */}
+          <div
+            className="absolute right-8 top-6"
+            onClick={() => setPasswordVisible((v) => !v)}
+          >
+            {passwordVisible ? (
+              <IoMdEyeOff className="text-6xl invert-50" />
+            ) : (
+              <IoMdEye className="text-6xl invert-50" />
+            )}
+          </div>
+          <p className="text-red-500 text-2xl text-left">
+            {errors.confirmPassword?.message}
+          </p>
         </div>
-        <p className="text-red-500 text-2xl text-left">
-          {errors.confirmPassword?.message}
-        </p>
-        <input
-          type={passwordVisible2 ? "text" : "password"}
-          name="confirmPassword"
-          id=""
-          placeholder="Confirm Password"
-          className=" border shadow rounded-xl py-8 mb-8 text-4xl placeholder:text-4xl px-5 placeholder:px-5 placeholder:text-gray-400 bg-white "
-          {...register("confirmPassword")}
-        />
+        <div className="w-full relative">
+          <input
+            type={passwordVisible2 ? "text" : "password"}
+            name="confirmPassword"
+            id=""
+            placeholder="Confirm Password"
+            className=" w-full border shadow rounded-xl py-8 mb-8 text-4xl placeholder:text-4xl px-5 placeholder:px-5 placeholder:text-gray-400 bg-white "
+            {...register("confirmPassword")}
+          />
 
-        {/* Hide Password for repeat password */}
-        <div
-          className="absolute right-8 top-145"
-          onClick={() => setPasswordVisible2((v) => !v)}
-        >
-          {passwordVisible2 ? (
-            <IoMdEyeOff className="text-6xl invert-50" />
-          ) : (
-            <IoMdEye className="text-6xl invert-50" />
-          )}
+          {/* Hide Password for repeat password */}
+          <div
+            className="absolute right-8 top-6"
+            onClick={() => setPasswordVisible2((v) => !v)}
+          >
+            {passwordVisible2 ? (
+              <IoMdEyeOff className="text-6xl invert-50" />
+            ) : (
+              <IoMdEye className="text-6xl invert-50" />
+            )}
+          </div>
         </div>
 
         <Button
